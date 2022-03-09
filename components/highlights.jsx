1adb88c1883232e0c6d28682b8700347bb960935
Alex Silva <9apsilva@gmail.com>
2022-03-09 00:38:03 -0300

    finished highlihts section

--- components/highlights.jsx
+++ components/highlights.jsx
@@ -1,11 +1,12 @@
-import { RichText } from 'prismic-reactjs';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getAllInDocument } from '../prismic/query';
 import styles from '../styles/HighLights.module.css'
 
 export default function HighLights() {
 
+    const carousselRef = useRef(null);
     const [highlights, setHighlights] = useState([])
+
     useEffect(() => {
         const fetchData = async () => {
             const dataInfoHighLights = await getAllInDocument('products');
@@ -14,22 +15,38 @@ export default function HighLights() {
         fetchData();
     }, []);
 
+    const clickArrowLeft = () => {
+        carousselRef.current.scrollLeft -= carousselRef.current.offsetWidth - (0.4*carousselRef.current.offsetWidth);
+    }
+    const clickArrowRight = () => {
+        carousselRef.current.scrollLeft += carousselRef.current.offsetWidth - (0.4*carousselRef.current.offsetWidth);
+    }
+
     return (
-        <section className={styles.body} id='highlights'>
-            <div className={styles.container}>
-                <div className={styles.carroussel}>
-                    {
-                        highlights.map((item) => {
-                            return (
-                                <ul key={item.uid} className={styles.item}>
-                                    <li><img alt={`imagem para ${item.uid}`} src={item.data.image.url} /></li>
-                                    <li className={styles.name}>{item.data.name[0].text}</li>
-                                </ul>
-                            );
-                        })
-                    }
+            <section className={styles.body} id='highlights'>
+                <div className={styles.container}>
+                    <div className={styles.caroussel} ref={carousselRef}>
+                        {
+                            highlights.map((item) => {
+                                return (
+                                    <ul key={item.uid} className={styles.item}>
+                                        <li><img alt={`imagem para ${item.uid}`} src={item.data.image.url} /></li>
+                                        <li className={styles.name}>{item.data.name[0].text}</li>
+                                    </ul>
+                                );
+                            })
+                        }
+                    </div>
+
+                    <div className={styles.arrows}>
+                        <button className={styles.arrow_left} onClick={clickArrowLeft}>
+                            <img src='/arrow-left.png' alt='button arrow left' style={{ width: '5rem', height: '5rem' }} />
+                        </button>
+                        <button className={styles.arrow_right} onClick={clickArrowRight}>
+                            <img src='/arrow-right.png' alt='button arrow right' style={{ width: '5rem', height: '5rem' }} />
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
     )
 }
