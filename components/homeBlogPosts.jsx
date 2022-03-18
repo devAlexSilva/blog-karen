544767bc4d9afdf08502ee9295e6713500bd2d04
Alex Silva <9apsilva@gmail.com>
2022-03-18 17:34:49 -0300

    change layout blog

--- components/homeBlogPosts.jsx
+++ components/homeBlogPosts.jsx
@@ -16,50 +16,53 @@ export default function HomeBlogPosts({ allPosts }) {
                 </div>
             </header>
 
-            <main className={styles.main}>
-                <h4>Mais Recentes</h4>
-                
-                <div className={styles.container}>
-                    {!router.isFallback ? <>
-                        <article className={styles.highlights}>
+            {!router.isFallback ? <>
+                <main className={styles.main}>
+                    <h4>Mais Recentes</h4>
+
+                    <article className={styles.highlights}>
+                        <div>
                             <ImageOfBlog
                                 alt={allPosts[0].slugs[0]}
                                 src={allPosts[0].data.image.url}
                                 height={1000}
                                 width={1000}
                             />
-                            <div><RichText render={allPosts[0].tags[0]} /></div>
-                            <div><RichText render={allPosts[0].data.name} /></div>
-                            <div><RichText render={allPosts[0].data.content} /></div>
-                        </article>
-                        <nav className={styles.nav_side}>
-                            {
-                                allPosts.map((item) => {
-                                    return (
-                                        <ul key={item.uid} className={styles.ul}>
-                                            <li><RichText render={item.data.name} /></li>
-                                            <li className={styles.img} style={{ cursor: 'pointer' }}>
-                                                <ImageOfBlog
-                                                    alt={`imagem para ${item.uid}`}
-                                                    src={item.data.image.url}
-                                                    height={300}
-                                                    width={300}
-                                                    uid={item.uid}
-                                                />
-                                            </li>
-                                            <li>{item.tags[0]}</li>
-                                        </ul>
-                                    )
-                                })
-                            }
-                        </nav>
-                    </> : <div style={{
-                        display: 'grid', placeItems: 'center',
-                        fontSize: '5rem', height: '90vh'
-                    }}>loading...</div>
+                        </div>
+                        <div><RichText render={allPosts[0].data.name} /></div>
+                        <div><RichText render={allPosts[0].data.content} /></div>
+                    </article>
+
+
+
+
+                </main>
+                <aside className={styles.aside}>
+                    {
+                        allPosts.map((item) => {
+                            return (
+                                <ul key={item.uid} className={styles.ul}>
+                                    <li><RichText render={item.data.name} /></li>
+                                    <li className={styles.img} style={{ cursor: 'pointer' }}>
+                                        <ImageOfBlog
+                                            alt={`imagem para ${item.uid}`}
+                                            src={item.data.image.url}
+                                            height={300}
+                                            width={300}
+                                            uid={item.uid}
+                                        />
+                                    </li>
+                                    <li>{item.tags[0]}</li>
+                                </ul>
+                            )
+                        })
                     }
-                </div>
-            </main>
+                </aside>
+            </> : <div style={{
+                display: 'grid', placeItems: 'center',
+                fontSize: '5rem', height: '90vh'
+            }}>loading...</div>
+            }
         </div>
     )
 }
